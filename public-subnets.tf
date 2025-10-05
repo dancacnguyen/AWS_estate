74aa9313a185e68da2d3de5aed035e0414965047
Matthew Barlund <100247852+matthewbarlund@users.noreply.github.com>
2025-10-05 16:23:27 -0700

    Add files via upload
    When testing this, I got an Error 403 from AWS, which is a good sign because it means these files can reach AWS. I wasn't sure how we'll set up the CLI wihtin the github; I figure we'll cross that bridge when we get there. I also plan to update this later with the locals.tf file in that same tutorial to make this code easier to update. I just wanted to get something out there for now

---
+++ public-subnets.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "public_zone1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.0.0/20"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name" = "dev-public-us-east-1a"
+        #here is also where we'd add EKS support if we wanted it
+    }
+}
+
+resource "aws_subnet" "public_zone2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.32.0/20"
+    availability_zone = "us-east-1b"
+    map_public_ip_on_launch = true
+
+    tags = {
+        "Name" = "dev-public-us-east-1b"
+        #here too for the EKS
+    }
+}
