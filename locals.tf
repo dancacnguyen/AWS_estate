15a7001a2f6083e4805a7a054204753f9f60e271
Matthew Barlund <100247852+matthewbarlund@users.noreply.github.com>
2025-10-08 15:19:53 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -6,4 +6,14 @@ locals {
     azs = ["us-east-1a", "us-east-1b"]
     public_subnets = ["10.0.0.0/20", "10.0.32.0/20"]
     
+    private_subnets = {
+        public_1 = {
+            cidr = "10.0.64.0/20"
+            az = "us-east-1a"
+        }
+        public_2 = {
+            cidr = "10.0.96.0/20"
+            az = "us-east-1b"
+        }
+    }
 }
