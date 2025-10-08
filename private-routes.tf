0897479556898bc1fc3ce5aa66d31b59d85354e0
Matthew Barlund <100247852+matthewbarlund@users.noreply.github.com>
2025-10-08 16:02:01 -0700

    Update private-routes.tf

--- private-routes.tf
+++ private-routes.tf
@@ -12,8 +12,9 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-    for_Each = local.private_subnets
+    for_each = local.private_subnets
 
     subnet_id = aws_subnet.private[each.key].id
     route_table_id = aws_route_table.private.id
 }
+
