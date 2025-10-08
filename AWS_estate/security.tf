f1c34272a0ed35667eb171ac358f654206e973c4
Rightgoody <rightgoody@csu.fullerton.edu>
2025-10-08 16:31:48 -0700

    Finished skeleton of security groups. TODO: define ingress and egress

--- AWS_estate/security.tf
+++ AWS_estate/security.tf
@@ -5,7 +5,7 @@ ensure a secure enviroment for our project!
 */
 
 
-// defining security group for application load balancer
+// defining security group for application load balancer (ALB)
 
 resource "aws_security_group" "alb_sg" {
     name        = "real-estate-alb-sg"
@@ -16,3 +16,26 @@ resource "aws_security_group" "alb_sg" {
         Name = "RealEstate-ALB-SG"
     }
 }
+
+// define security groups for web servers (EC2)
+resource "aws_security_group" "web_sg" {
+    name        = "real-estate-web-sg"
+    description = "Security group for the web server EC2 instances"
+    vpc_id      = aws_vpc.main.id
+
+    tags= {
+        Name = "RealEstate-Web-SG"
+    }
+}
+
+//define security groups for RDS db
+resource "aws_security_group" "rds_sg" {
+    name        = "real-estate-rds-sg"
+    description = "Security group for the RDS database instance"
+    vpc_id      = aws_vpc.main.id
+
+    tags = {
+        Name = "RealEstate-RDS-SG"
+    }
+}
+
